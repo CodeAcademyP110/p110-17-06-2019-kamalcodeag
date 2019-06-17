eaff474bb5f5fff9823b224b4c79fa74ff6e7eed
Kamal Guliyev <kamalag@code.edu.az>
2019-06-18 03:48:29 +0400

    *** UNSOLVED ATM HomeTask with delegate and events ***

---
+++ HomeTask/HomeTask/HomeTask/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask
+{
+    //public delegate void TestDelegate(int number);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Practice Delegate
+            //Testing testing = new Testing();
+
+            //1st way
+            //TestDelegate del1 = new TestDelegate(testing.ShowNumber);
+            //del1(7); //7
+            //TestDelegate del2 = new TestDelegate(testing.GetSum);
+            //del2(8); //16
+
+            //2nd way
+            //TestDelegate del3 = testing.ShowNumber;
+            //del3 += testing.GetSum;
+            //del3(7);
+            //del3(8);
+            #endregion
+
+            #region HomeTask
+            ATM atm = new ATM();
+
+            atm.OnBalanceLow += BalanceChangedAfterWithdraw;
+            atm.OnBalanceUp += BalanceChangedAfterDeposit;
+            atm.OnBalanceFinished += BalanceFinished;
+
+            ATM.Withdraw(50);
+            ATM.Deposit(200);
+            ATM.Withdraw(1150);
+            #endregion
+        }
+
+        public static void BalanceChangedAfterWithdraw(object sender, EventArgs e)
+        {
+            Console.WriteLine("Withdraw process occured");
+        }
+        public static void BalanceChangedAfterDeposit(object sender, EventArgs e)
+        {
+            Console.WriteLine("Deposit process occured");
+        }
+        public static void BalanceFinished(object sender, EventArgs e)
+        {
+            Console.WriteLine("You do not have sufficient money in your balance");
+        }
+    }
+
+    #region Practice Delegate
+    //public class Testing
+    //{
+    //    public void ShowNumber(int number)
+    //    {
+    //        Console.WriteLine(number);
+    //    }
+
+    //    public void GetSum(int number)
+    //    {
+    //        Console.WriteLine(number + number);
+    //    }
+    //}
+    #endregion
+
+    #region HomeTask
+    public class ATM
+    {
+        public static decimal Balance { get; private set; } = 1000; // 1000 AZN
+        public delegate void MainDelegate(object sender, EventArgs e);
+        public event MainDelegate OnBalanceLow;
+        public event MainDelegate OnBalanceUp;
+        public event MainDelegate OnBalanceFinished;
+
+        public static void Withdraw(decimal money)
+        {
+            if (money > 0 && Balance >= money)
+            {
+                Balance -= money;
+                Console.WriteLine(Balance);
+            }
+        }
+        public static void Deposit(decimal money)
+        {
+            if (money > 0)
+            {
+                Balance += money;
+                Console.WriteLine(Balance);
+            }
+        }
+    }
+    #endregion
+}
---
+++ P110_ConsoleDemo/P110_ConsoleDemo/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P110_ConsoleDemo
+{
+    class Program
+    {
+        public delegate bool IntCheckers(int item);
+        public delegate void SendVerification(string verifyMessage);
+
+        //enum Colors { red,black,yellow}
+
+        static void Main(string[] args)
+        {
+            //foreach (var item in Enum.GetNames(typeof(Colors)))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #region delegate
+            //Add(IsEven, 10, 15, 20, 35, 56, 89, 78);
+            //Add(IsOlder, 25, 10, 45, 56, 98, 11);
+
+            //SendVerification sendVerification = new SendVerification(WriteToConsole);
+            //SendVerification sendVerification = SendSMS;
+            //sendVerification += SendEmail;
+            //sendVerification += SendSMS;
+            //sendVerification += SendSMS;
+            //sendVerification += WriteToConsole;
+
+            //sendVerification -= WriteToConsole;
+            //sendVerification -= WriteToConsole;
+            //sendVerification -= WriteToConsole;
+            //sendVerification -= WriteToConsole;
+            //sendVerification -= WriteToConsole;
+
+            //sendVerification("100 AZN deducted.");
+            #endregion
+
+            //ATM mainATM = new ATM();
+            //ATM azadliqATM = new ATM();
+
+            //mainATM.OnBalanceLow += SendSms;
+            //mainATM.OnBalanceLow += Write;
+
+            ////anonymous method
+            //azadliqATM.OnBalanceLow += delegate (object sender, ATMEventArgs e)
+            //{
+            //    ATM atm = (ATM)sender;
+            //    Console.WriteLine($"Balance is low on ATM №{atm.ID}. Current amount is {e.CurrentBalance} at {e.ActionTime.ToShortDateString()}");
+            //};
+
+            ////lambda expression
+            //azadliqATM.OnBalanceLow += (sender, e) =>
+            //{
+            //    ATM atm = (ATM)sender;
+            //    Console.WriteLine($"Caution. Balance azalir ATM №{atm.ID}. Faktiki balans: {e.CurrentBalance}, tarix: {e.ActionTime.ToLongDateString()}");
+            //};
+
+            //mainATM.TopUp(2000);
+            //mainATM.Withdraw(700);
+            //mainATM.Withdraw(500);
+
+            //azadliqATM.TopUp(1300);
+            //azadliqATM.Withdraw(550);
+        }
+
+        class ATM
+        {
+            private static int id_counter = 1;
+            private int _id;
+
+            public int ID
+            {
+                get { return _id; }
+                private set { _id = value; }
+            }
+
+            public ATM()
+            {
+                _id = id_counter++;
+            }
+
+            public delegate void BalanceLow(object sender, ATMEventArgs e);
+
+            public event BalanceLow OnBalanceLow;
+
+            public decimal Balance { get; private set; }
+
+            public void Withdraw(decimal amount)
+            {
+                if (Balance >= amount) Balance -= amount;
+                Console.WriteLine($"Balans-dan {amount} azn pul cixidli. Balans: {Balance}");
+                //else 
+
+                //check whether balance is low
+                if (Balance <= 1000)
+                {
+                    //if(OnBalanceLow != null)
+                    //    OnBalanceLow();
+
+                    OnBalanceLow?.Invoke(this, new ATMEventArgs
+                    {
+                        CurrentBalance = Balance,
+                        ActionTime = DateTime.Now
+                    });
+                }
+            }
+
+            public void TopUp(decimal amount)
+            {
+                Balance += amount;
+            }
+
+
+        }
+
+        class ATMEventArgs : EventArgs
+        {
+            public decimal CurrentBalance { get; set; }
+            public DateTime ActionTime { get; set; }
+        }
+
+
+
+        static void SendSms(object sender, ATMEventArgs e)
+        {
+            ATM atm = (ATM)sender;
+            Console.WriteLine($"ID-si {atm.ID} olan Atm-de faktiki balans {e.CurrentBalance} AZN. Bas verme tarixi: {e.ActionTime}");
+        }
+
+        static void Write(object sender, ATMEventArgs e)
+        {
+            ATM atm = (ATM)sender;
+            Console.WriteLine($"ID-si {atm.ID} olan ATM-e balans azaldi. Balansin miqdari: {e.CurrentBalance}, Bas verme tarixi: {e.ActionTime}");
+        }
+
+
+        static void WriteToConsole(string sentence)
+        {
+            Console.WriteLine(sentence);
+        }
+
+        static void SendEmail(string sentence)
+        {
+            //email sending simulation
+            Console.WriteLine("Email sent successfully");
+        }
+
+        static void SendSMS(string message)
+        {
+            //message sent simulation
+            Console.WriteLine("SMS sent successfully");
+        }
+
+
+
+
+
+        static int Add(IntCheckers callBack, params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int item in numbers)
+            {
+                if (callBack(item))
+                    sum += item;
+            }
+            return sum;
+        }
+
+        static bool IsEven(int number) => number % 2 == 0;
+
+        static bool IsOlder(int age) => age > 18;
+    }
+
+   
+
+}
+
